d5d1a354326c3a53b1dc2fd39672516a748c71ff
Marioditrani <marioditrani2003@gmail.com>
2023-10-11 10:28:15 +0200

    first commit

---
+++ src/components/AppNav.vue
@@ -0,0 +1,69 @@
+<script>
+
+</script>
+
+<template>
+    <div class="header">
+        <div class="logo">
+            <span class="logo-up">federalist</span>
+            <span class="logo-down">pig</span>
+        </div>
+        <ul class="nav">
+            <li class="menu"><strong>menu</strong></li>
+            <li class="catering"><strong>catering</strong></li>
+            <li class="locations"><strong>locations</strong></li>
+            <li class="bbq-box"><strong>bbq box</strong></li>
+            <li class="supper-club"><strong>supper club</strong></li>
+            <li class="gift-cards"><strong>gift cards</strong></li>
+            <li class="orders-online"><strong>orders online</strong></li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+*{
+    text-transform: uppercase;
+}
+.header{
+    background-color: #ffd700;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.logo{
+   display: flex;
+   flex-direction: column;
+   font-size: 45px;
+   padding: 5px 30px;
+   
+}
+
+.nav{
+    padding: 0px 30px;
+    height: 100%;
+    display: flex;
+    gap: 20px;
+    font-size: 16px;
+}
+
+.menu, .catering, .locations, .bbq-box, .supper-club, .gift-cards, .orders-online{
+    padding: 7px 5px;
+}
+.menu:hover, .catering:hover, .locations:hover, .bbq-box:hover, .supper-club:hover, .gift-cards:hover{
+    background-color: black;
+    color: #ffd700;
+    padding: 7px 5px;
+    transition: all  linear 0.2s ;
+    
+}
+.orders-online{
+    border: 2px solid black;
+    padding: 5px 10px;
+}
+.orders-online:hover{
+    padding: 5px 10px;
+    background-color: black;
+    color: #ffd700;
+    transition: all  linear 0.2s ;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,88 @@
+<script>
+export default{
+    data(){
+        return{
+            obs:0,
+            slides:[
+            "https://images.getbento.com/accounts/22022076ca31682d32e6a3253b8e9e7c/media/images/70222220826_FedPig_B1_Traditional4_0401.jpg?w=1200&fit=crop&auto=compress,format&crop=focalpoint&fp-x=0.5&fp-y=0.5",
+            "https://images.getbento.com/accounts/22022076ca31682d32e6a3253b8e9e7c/media/images/67494IMG_2648.JPG?w=1200&fit=crop&auto=compress,format&crop=focalpoint&fp-x=0.44&fp-y=0.66",
+            "https://images.getbento.com/accounts/22022076ca31682d32e6a3253b8e9e7c/media/images/41145federalist-pig-home-pattern-black.jpg?w=1200&fit=crop&auto=compress,format&crop=focalpoint&fp-x=0.5&fp-y=0.5",
+            "https://images.getbento.com/accounts/22022076ca31682d32e6a3253b8e9e7c/media/images/6865220826_FedPig_A3_FavTraditional_0110.jpg?w=1200&fit=crop&auto=compress,format&crop=focalpoint&fp-x=0.48&fp-y=0.98",
+            "https://images.getbento.com/accounts/22022076ca31682d32e6a3253b8e9e7c/media/images/95907IMG_6837.JPG?w=1200&fit=crop&auto=compress,format&crop=focalpoint&fp-x=0.5&fp-y=0.5",
+            ]
+        };
+    },
+    methods: {
+        primaImg(){
+            this.obs = 0
+        },
+        secondaImg(){
+             this.obs = 1
+        },
+        terzaImg(){
+             this.obs = 2
+        },
+        quartaImg(){
+             this.obs = 3
+        },
+        quintaImg(){
+             this.obs = 4
+        },
+    }
+}
+
+</script>
+
+<template>
+    <div class="carousel">
+        <div class="carousel-image" v-for="(slide, i) in slides" :key="slide">
+            <img v-if="i == obs" :src="slides[i]" >
+        </div>
+    </div>
+    <div class="carousel-button">
+        <div class="button1" :class="obs == 0 ? 'btn-active' : ''" @click="primaImg"></div>
+        <div class="button2" :class="obs == 1 ? 'btn-active' : ''" @click="secondaImg"></div>
+        <div class="button3" :class="obs == 2 ? 'btn-active' : ''" @click="terzaImg"></div>
+        <div class="button4" :class="obs == 3 ? 'btn-active' : ''" @click="quartaImg"></div>
+        <div class="button5" :class="obs == 4 ? 'btn-active' : ''" @click="quintaImg"></div>
+    
+    </div>
+</template>
+
+<style scoped>
+.carousel{
+    
+}
+.carousel-image{
+    
+}
+.carousel-image img{
+    width: 100%;
+    
+}
+.carousel-button{
+    gap: 13px;
+    display: flex;
+    background-color: #ffd700;
+    padding: 17px;
+    max-width: 350px;
+    margin: 10px;
+    align-items: center;
+    justify-content: center;
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
+}
+.button1, .button2, .button3, .button4, .button5{
+    width: 13px;
+    height: 13px;
+    border-radius: 50%;
+    background-color: black;
+    opacity: .65;
+}
+.button1:hover, .button2:hover, .button3:hover, .button4:hover, .button5:hover{
+    opacity: 1;
+}
+.btn-active{
+    opacity: 1;
+}
+</style>
